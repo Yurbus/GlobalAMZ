0e01c59e6a895ad24dd513cd2b90420d7f604047
Yurbus <frankins636@gmail.com>
2024-12-10 09:59:10 +0200

    ddd

--- assets/js/sw-slider.js
+++ assets/js/sw-slider.js
@@ -51,3 +51,31 @@ var swiper = new Swiper(".mySwiper2", {
         },
     },
 });
+var swiper = new Swiper(".mySwiper3", {
+    cssMode: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+        580: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        890: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+});
